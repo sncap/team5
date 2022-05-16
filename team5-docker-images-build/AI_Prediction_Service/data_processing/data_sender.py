13acf71949a9cbf1f67c56b39211f667aa2b1650
root <root@architect22-5team-m.us-west1-b.c.architect-certification-289902.internal>
2022-05-16 08:23:05 +0000

    update

--- team5-docker-images-build/AI_Prediction_Service/data_processing/data_sender.py
+++ team5-docker-images-build/AI_Prediction_Service/data_processing/data_sender.py
@@ -1,5 +1,4 @@
-import http.client
-import json
+import requests
 
 class DataSender:
     def __init__(self, config):
@@ -7,28 +6,15 @@ class DataSender:
         self.port = config['sport']
         self.model = config['model']
         self.url = config['url']
-        self.conn = self.connect()
-
-    def connect(self):
-        conn = http.client.HTTPConnection(host=self.ip, port=self.port, timeout=1)
-        return conn
 
     def predict(self, msg):
         try:
             HOST = '{}.myspace.example.com'.format(self.model)
             headers = {'Content-type': 'application/json', 'Host': HOST}
-            self.conn.request('POST', self.url, msg, headers, )
-            response = self.conn.getresponse()
+            address = '{}:{}{}'.format(self.ip, self.port, self.url)
+            response = requests.post(address, data=msg, headers=headers)
         except Exception as e:
             response = None
         return response
 
-
-if __name__ == '__main__':
-    config_file = '../config/data_processing_config.json'
-    with open(config_file, 'r') as cf:
-        config = json.load(cf)
-        print("Config " + str(config))
-
-    sender_instance = DataSender(config['cifar10_predict_server_config'])
 
